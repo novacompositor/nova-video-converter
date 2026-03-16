3031bb3575fa79643a17c339e804a419e342f0b6
Nova Compositor Team <team@novacompositor.app>
2026-03-16 20:03:17 +0500

    style(ci): cargo fmt to pass CI checks

--- app_bridge/build.rs
+++ app_bridge/build.rs
@@ -1,5 +1,7 @@
 fn main() {
-    cxx_build::bridge("src/lib.rs").flag_if_supported("-std=c++14").compile("app_bridge_cxx");
+    cxx_build::bridge("src/lib.rs")
+        .flag_if_supported("-std=c++14")
+        .compile("app_bridge_cxx");
 
     println!("cargo:rerun-if-changed=src/lib.rs");
 }
